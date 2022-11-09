b21e10860d08d761cc88b3cc978a075ec2353052
kkkimjiyong <113953473+kkkimjiyong@users.noreply.github.com>
2022-11-10 00:03:58 +0900

    Update CurrentLocation.js

--- Location/CurrentLocation.js
+++ Location/CurrentLocation.js
@@ -1,1 +1,23 @@
-dd;
+ //아래는 네비게이터를 실행할 때, 넣어주는 옵션 
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  //내 위치를 불러오는 것에 성공했을 때 , 실행되는 함수
+  function success(position) {
+    //현재 위치를 state값으로
+    SetMyaddress({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+  //불러오지 못했을 때 실행되는 함수.
+  function error(err) {
+    console.warn("ERROR(" + err.code + "): " + err.message);
+  }
+  
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  },[])
